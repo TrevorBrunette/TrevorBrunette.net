e52946b45acaaf07941b62b3bb8e1282c6c04e05
Trevor Brunette <trevorbrunette@gmail.com>
2024-02-25 22:16:10 -0700

    About page created

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,14 @@
+import "./index.scss";
+
+function Contact() {
+  return (
+    <div className="container contact-page">
+      <div className="text-zone">
+        <h1>Contact Me</h1>
+        <h2>This page is a work in progress</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
